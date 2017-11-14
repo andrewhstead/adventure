ec55656de4a80f6d2d260cd0c051fc61a09cba07
Andrew Stead <andrew@andrewstead.co.uk>
2017-11-14 14:31:54 +0000

    Addition of descriptions and objects to locations

--- adventure.py
+++ adventure.py
@@ -8,21 +8,40 @@ directions = {
 }
 
 position = (0, 0)
+carrying = []
 
 while True:
-    location = locations[position]
-    print 'you are at the %s' % location
+    location = locations[position]['name']
+    description = locations[position]['description']
+    object = locations[position]['object']
+    print 'You are at the %s.' % location
+    print '%s' % description
+
+    action = raw_input('Do you want to take the %s? Enter yes/no.' % object)
+
+    if action == 'yes':
+        carrying.append(object)
+        print 'You take the %s.' % object
+        print 'You have with you:' + str(carrying)
+    elif action == 'no':
+        print 'You leave the %s behind.' % object
+        if len(carrying) == 0:
+            print 'You have with you: nothing'
+        else:
+            print 'You have with you:' + str(carrying)
+    else:
+        print 'Sorry, I don\'t understand. Please enter yes/no.'
 
     valid_directions = {}
     for k, v in directions.iteritems():
         possible_position = (position[0] + v[0], position[1] + v[1])
         possible_location = locations.get(possible_position)
         if possible_location:
-            print 'to the %s is a %s' % (k, possible_location)
+            print 'To the %s is a %s.' % (k, possible_location['name'])
             valid_directions[k] = possible_position
 
-    direction = raw_input('which direction do you want to go?\n')
+    direction = raw_input('Which direction do you want to go?\n')
     if direction in valid_directions:
         position = valid_directions[direction]
     else:
-        print 'you cannot go that way!'
+        print 'You cannot go that way! Choose a different direction.'
--- data.py
+++ data.py
@@ -1,6 +1,22 @@
 locations = {
-    (0, 0): 'house',
-    (0, 1): 'lake',
-    (1, 0): 'park',
-    (1, 1): 'market',
+    (0, 0): {
+        'name': 'house',
+        'description': 'The house is deserted, but there is a loaf of bread on an old table in the front room.',
+        'object': 'bread'
+    },
+    (0, 1): {
+        'name': 'lake',
+        'description': 'The lake is full of fish and has several ducks swimming on it, as well as one lost penguin.',
+        'object': 'penguin'
+    },
+    (1, 0): {
+        'name': 'park',
+        'description': 'People are walking their dogs in the park, while a lamb is grazing near to an empty bench.',
+        'object': 'lamb'
+    },
+    (1, 1): {
+        'name': 'market',
+        'description': 'The market is full of cattle which are for sale, including one very hungry looking cow.',
+        'object': 'cow'
+    },
 }
