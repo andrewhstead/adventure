5f3b2f92705a295f9a431469b0632dfb338fe5ef
Andrew Stead <andrew@andrewstead.co.uk>
2017-11-15 10:54:32 +0000

    added ability to put objects down in a different place

--- adventure.py
+++ adventure.py
@@ -1,4 +1,4 @@
-from data import locations
+from data import locations, objects
 
 directions = {
     'west': (-1, 0, 0),
@@ -14,24 +14,24 @@ carrying = []
 
 
 def pick_up_object():
-    action = raw_input('Do you want to take the %s? Enter yes/no.\n' % object)
+    action = raw_input('Do you want to take the %s? Enter yes/no.\n' % object_name)
     if action == 'yes':
         if len(carrying) >= 4:
             print 'You are carrying too much. You need to put something back where you found it!'
         else:
-            carrying.append(object)
-            if object == 'candyfloss':
+            carrying.append(object_name)
+            if object_name == 'candyfloss':
                 print 'That is disgusting. Why would you pick that up? But OK, you\'re in charge!'
-            elif object == 'rat':
+            elif object_name == 'rat':
                 print 'Seriously? Do you have any idea how many diseases it might be carrying? Be very careful!'
-            print 'You take the %s.' % object
+            print 'You take the %s.' % object_name
             print 'You are carrying:' + str(carrying)
-            if object == 'cow' and 'bread' in carrying or object == 'bread' and 'cow' in carrying:
+            if object_name == 'cow' and 'bread' in carrying or object_name == 'bread' and 'cow' in carrying:
                 print 'The cow is licking the bread. I wouldn\'t advise making a sandwich...'
-            if object == 'penguin' and 'cow' in carrying or object == 'cow' and 'penguin' in carrying:
+            if object_name == 'penguin' and 'cow' in carrying or object_name == 'cow' and 'penguin' in carrying:
                 print 'The cow and the penguin are whispering to each other. They are clearly up to something...'
     elif action == 'no':
-        print 'You leave the %s behind.' % object
+        print 'You leave the %s where it is.' % object_name
         if len(carrying) == 0:
             print 'You are not carrying anything.'
         else:
@@ -42,34 +42,37 @@ def pick_up_object():
 
 
 def put_down_object():
-    action = raw_input('Do you want to put down the %s? Enter yes/no.\n' % object)
+    action = raw_input('Do you want to put down the %s? Enter yes/no.\n' % object_name)
     if action == 'yes':
-        carrying.remove(object)
-        print 'You put down the %s.' % object
+        carrying.remove(object_name)
+        print 'You put down the %s.' % object_name
         if len(carrying) == 0:
             print 'You are not carrying anything.'
         else:
             print 'You are carrying:' + str(carrying)
     elif action == 'no':
-        print 'You have with you:' + str(carrying)
+        print 'You are carrying:' + str(carrying)
     else:
         print 'Sorry, I don\'t understand. Please enter yes/no.'
         put_down_object()
 
 while True:
-    place = locations[position]['place']
-    place_desc = locations[position]['place_desc']
-    object = locations[position]['object']
-    object_desc = locations[position]['object_desc']
-    print 'You are at the %s.' % place
-    print '%s' % place_desc
-    if object not in carrying:
-        print '%s' % object_desc
-        pick_up_object()
+    place_name = locations[position]['place']
+    place_desc = locations[position]['description']
 
-    else:
-        print 'You have with you:' + str(carrying)
-        put_down_object()
+    print 'You are at the %s.' % place_name
+    print '%s' % place_desc
+
+    for k, v in objects.iteritems():
+        if v['location'] == position:
+            object_name = objects[k]['name']
+            object_desc = objects[k]['description']
+
+            if object_name not in carrying:
+                print '%s' % object_desc
+                pick_up_object()
+            else:
+                put_down_object()
 
     valid_directions = {}
     for k, v in directions.iteritems():
@@ -82,5 +85,9 @@ while True:
     direction = raw_input('Which direction do you want to go?\n')
     if direction in valid_directions:
         position = valid_directions[direction]
+        for k, v in objects.iteritems():
+            if k in carrying:
+                v['location'] = position
+                v['description'] = 'There is a ' + v['name'] + ' here which somebody has left behind. Was that you?!'
     else:
         print 'You cannot go that way! Choose a different direction.'
--- data.py
+++ data.py
@@ -1,50 +1,77 @@
 locations = {
     (0, 0, 0): {
         'place': 'house',
-        'place_desc': 'The house is deserted, all the windows are broken and the door is hanging off its hinges.',
-        'object': 'bread',
-        'object_desc': 'There is a loaf of bread on an old table in the front room.'
+        'description': 'The house is deserted, all the windows are broken and the door is hanging off its hinges.'
     },
     (0, 0, 1): {
         'place': 'rooftop',
-        'place_desc': 'The rooftop is hazardous, with broken slates everywhere and a crumbling chimney pot.',
-        'object': 'football',
-        'object_desc': 'There is a football resting in the gutter, which looks like it has been here a long time.'
+        'description': 'The rooftop is hazardous, with broken slates everywhere and a crumbling chimney pot.'
     },
     (0, 1, 0): {
         'place': 'lake',
-        'place_desc': 'The lake is full of fish and has several ducks swimming on it.',
-        'object': 'penguin',
-        'object_desc': 'There is one lost penguin in the lake as well, which seems to have aspirations to be a duck.'
+        'description': 'The lake is full of fish and has several ducks swimming on it.'
     },
     (-1, 1, 0): {
         'place': 'church',
-        'place_desc': 'The church is very quiet, with one person sitting alone in the front pew.',
-        'object': 'cat',
-        'object_desc': 'There is a cat prowling around, on the lookout for any mice that might appear.'
+        'description': 'The church is very quiet, with one person sitting alone in the front pew.'
     },
     (1, 0, 0): {
         'place': 'park',
-        'place_desc': 'There are children playing in the park, and lots of people walking their dogs.',
-        'object': 'lamb',
-        'object_desc': 'There is a lamb grazing near to the bandstand, doing an excellent job of mowing the grass.'
+        'description': 'There are children playing in the park, and lots of people walking their dogs.'
     },
     (1, 0, -1): {
         'place': 'sewer',
-        'place_desc': 'The sewer is very dark and smells horrendous - still, what on earth were you expecting?!',
-        'object': 'rat',
-        'object_desc': 'There is a very large rat nearby, with enormous front teeth.'
+        'description': 'The sewer is very dark and smells horrendous - still, what on earth were you expecting?!'
     },
     (1, 1, 0): {
         'place': 'market',
-        'place_desc': 'The market is very busy, with pens full of horses, sheep and cattle which are for sale.',
-        'object': 'cow',
-        'object_desc': 'There is one small cow standing on its own, looking very hungry.'
+        'description': 'The market is very busy, with pens full of horses, sheep and cattle which are for sale.'
     },
     (1, 2, 0): {
         'place': 'fairground',
-        'place_desc': 'The fairground is full of rides and loud music is playing all around.',
-        'object': 'candyfloss',
-        'object_desc': 'There is some half-eaten candyfloss on the floor, with bits of grass stuck in it.'
+        'description': 'The fairground is full of rides and loud music is playing all around.'
+    },
+}
+
+objects = {
+    'loaf of bread': {
+        'name': 'loaf of bread',
+        'location': (0, 0, 0),
+        'description': 'There is a loaf of bread on an old table in the front room.'
+    },
+    'football': {
+        'name': 'football',
+        'location': (0, 0, 1),
+        'description': 'There is a football resting in the gutter, which looks like it has been here a long time.'
+    },
+    'penguin': {
+        'name': 'penguin',
+        'location': (0, 1, 0),
+        'description': 'There is one lost penguin in the lake as well, which seems to have aspirations to be a duck.'
+    },
+    'cat': {
+        'name': 'cat',
+        'location': (-1, 1, 0),
+        'description': 'There is a cat prowling around, on the lookout for any mice that might appear.'
+    },
+    'lamb': {
+        'name': 'lamb',
+        'location': (1, 0, 0),
+        'description': 'There is a lamb grazing near to the bandstand, doing an excellent job of mowing the grass.'
+    },
+    'rat': {
+        'name': 'rat',
+        'location': (1, 0, -1),
+        'description': 'There is a very large rat nearby, with enormous front teeth.'
+    },
+    'cow': {
+        'name': 'cow',
+        'location': (1, 1, 0),
+        'description': 'There is one small cow standing on its own, looking very hungry.'
+    },
+    'candyfloss': {
+        'name': 'candyfloss',
+        'location': (1, 2, 0),
+        'description': 'There is some half-eaten candyfloss on the floor, with bits of grass stuck in it.'
     },
 }
